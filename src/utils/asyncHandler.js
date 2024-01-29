2703a3d29fb77ccaec4bdf140a1908c9c73dc664
yashwardhan pathak <pathakyashwardhan54@gmail.com>
2024-01-29 15:40:40 +0530

    all setup configures

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,6 +1,6 @@
 const asyncHandler= (requestHandler)=>{
-   (req,res,next)=>{
-    Promise.resolve(requestHandler()).catch((err)=>next(err))
+   return (req,res,next)=>{
+    Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
    }
 }
 
@@ -22,3 +22,5 @@ export {asyncHandler}
 //     })
 // }
 // }
+
+// export {asyncHandler}
